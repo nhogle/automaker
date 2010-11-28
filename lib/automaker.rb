bcf6b9ed7bcff2666a4d4928af6b90a6bc29e691
Micah Geisel <micah@botandrose.com>
2010-11-27 22:29:00 -0800

    refactor options to a hash.

--- lib/automaker.rb
+++ lib/automaker.rb
@@ -2,23 +2,27 @@ require 'rubygems'
 require 'rb-inotify'
 
 class Automaker
-  def run(args)
-    @path_to_watch = args.shift || "."
-    @path_to_build = args.shift || "./build"
-    @filters = args
+  def self.run(args)
+    new :path_to_watch => args.shift || ".",
+      :path_to_build => args.shift || "./build",
+      :filters => args
+  end
+
+  def initialize(options)
+    @options = options
     run_stream
     0
   end
 
   def print_usage
-   $stderr.puts "Usage: automaker </path/to/watch> </path/to/build> [filter [filter [ etc.. ]]]
+    $stderr.puts "Usage: automaker [/path/to/watch] [/path/to/build] [filter [filter [ etc.. ]]]
 You must specify the path to watch. Make is only triggered if a file whose name
 one of the filters is changed. (Otherwise you will likely enter an infinite loop.)"
   end
 
   def run_stream
     notifier = INotify::Notifier.new
-    notifier.watch @path_to_watch, :modify do |event|
+    notifier.watch path_to_watch, :modify do |event|
       make if should_make [event.name]
     end
     notifier.run
@@ -26,11 +30,24 @@ one of the filters is changed. (Otherwise you will likely enter an infinite loop
   
   def should_make(modified_files)
     modified_files.any? do |filename|
-      @filters.empty? or @filters.any? { |filter| filename.include?(filter) }
+      filters.empty? or filters.any? { |filter| filename.include?(filter) }
     end
   end
   
   def make
-    system("cd #{@path_to_build} && make")
+    system "cd #{path_to_build} && make"
   end
+
+  private
+    def path_to_watch
+      @options[:path_to_watch]
+    end
+
+    def path_to_build
+      @options[:path_to_build]
+    end
+
+    def filters
+      @options[:filters]
+    end
 end
